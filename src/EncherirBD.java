d5af21ee8418beb9edc6e5755b68830a0edcd7b2
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-19 09:28:41 +0200

    insere la methode insereEncherir

--- src/EncherirBD.java
+++ src/EncherirBD.java
@@ -11,19 +11,12 @@ public class EncherirBD {
     public EncherirBD(ConnexionMySQL laConnexionMySQL) {
         this.laConnexionMySQL = laConnexionMySQL;
     }
-    public void insereVente(Vente v) {
-
-
-
-
-        PreparedStatement s = this.laConnexionMySQL.preparedStatement("INSERT INTO VENTE VALUES (?,?,?,?,?,?,?)");
-        s.setDouble(1, v.getIdVente());
-        s.setDouble(2, v.getPrixBase());
-        s.setDouble(3, v.getPrixMin());
-        s.setDate(4, v.getdebutVente());
-        s.setDate(5, v.getfinVe());
-        s.setDouble(6, 0.);
-        s.setString(7, null);
+    public insereEnchere(Enchere e,Utilisateur u, Vente v){
+        PreparedStatement s = this.laConnexionMySQL.preparedStatement("INSERT INTO ENCHERIR VALUES (?,?,?,?)");
+        s.setDouble(1, u.getIdUtilisateur());
+        s.setDouble(2, v.getIDVente());
+        s.setDate(0, e.getDateHeure());
+        s.setDouble(4, e.getMontant());
         s.executeQuery();
     }
 
