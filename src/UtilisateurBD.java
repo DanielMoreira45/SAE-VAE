c2eadc720a5ff43678a35adb0c0d429e3880e6a2
alexandre <o22205312@info26-11.iut45.univ-orleans.fr>
2023-06-19 09:31:29 +0200

    insert utilisateur

--- src/UtilisateurBD.java
+++ src/UtilisateurBD.java
@@ -1,3 +1,5 @@
+import java.sql.PreparedStatement;
+
 /**
  * 
  */
@@ -9,5 +11,40 @@ public class UtilisateurBD {
     public UtilisateurBD(ConnexionMySQL laConnexionMySQL) {
         this.laConnexionMySQL = laConnexionMySQL;
     }
+
+
+    int maxIdUtilisateur() throws SQLException {
+		this.st = laConnexion.createStatement();
+		this.resultats = this.st.executeQuery("SELECT max(idut) FROM UTILISATEUR;");
+		resultats.next();
+		int nb = resultats.getInt(1);
+		this.resultats.close();
+		return nb;
+	}
+
+
+    public int insererUtilisateur(Utilisateur j, Role role) throws SQLException {
+		PreparedStatement ps = laConnexion.prepareStatement("INSERT INTO JOUEUR VALUES(?, ?, ?, ?, ?, ?)");
+		ps.setInt((1),  maxIdJoueur() + 1);
+		ps.setString(2, j.getPseudo());
+		ps.setString(3, j.getEmail());
+		ps.setString(4, j.getMotDePasse());
+        ps.setChar(5, j.estActive());
+        ps.setInt(6, role);
+		ps.executeUpdate();
+	}
+
+
+    
+
+
+
+
+
+    
+
+
+
+
 
 }
