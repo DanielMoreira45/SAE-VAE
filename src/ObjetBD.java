dcd397aea1dee43b2e771a9bcd374ceaece0a562
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-19 09:28:25 +0200

    insere la methode insereObjet

--- src/ObjetBD.java
+++ src/ObjetBD.java
@@ -1,3 +1,5 @@
+import java.sql.PreparedStatement;
+
 /**
  * 
  */
@@ -8,6 +10,15 @@ public class ObjetBD {
      */
     public ObjetBD(ConnexionMySQL laConnexionMySQL) {
         this.laConnexionMySQL = laConnexionMySQL;
+    }
+    public void insereObjet(Objet o,Utilisateur u, Categorie cat){
+        PreparedStatement s = this.laConnexionMySQL.preparedStatement("INSERT INTO OBJET VALUES (?,?,?,?,?)");
+        s.setDouble(1, o.getidObjet());
+        s.setString(2, o.getNomObjet());
+        s.setString(3, o.getDescription());
+        s.setDouble(4, u.getIdUtilisateur());  
+        s.setDouble(5, cat);
+        s.executeQuery();
     }
 
 }
