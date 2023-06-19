991bcc06e0a25bbdca0980226cbae80f41f4785b
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-19 10:51:42 +0200

    ajout des valeurs dans les enums

--- src/Status.java
+++ src/Status.java
@@ -1,6 +1,10 @@
 
 /**
- * 
+ * Permet d'avoir le Roles de chaque utilisateur
  */
-public enum Status {
+public class Status {
+    private Status(){}
+    public static int ADMINISTRATEUR = 1;
+    public static int UTILISATEUR = 2;
+
 }
