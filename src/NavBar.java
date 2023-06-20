88fae96a226fa75d04378e0dc43a51311d4a0269
Rémi Boulay <remi.boulay12@gmail.com>
2023-06-20 07:00:52 +0000

    Téléverser un nouveau fichier

---
+++ src/NavBar.java
@@ -0,0 +1,120 @@
+import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.geometry.Insets;
+
+
+
+public class NavBar extends Application{
+
+    @Override
+    public void init() {
+
+    }
+
+    public void start(Stage stage) throws Exception{
+        // Construction du graphe de scène
+        BorderPane root = new BorderPane();
+        Scene scene = new Scene(root);
+        stage.setTitle("Fenetre Acceuil");
+        stage.setFullScreen(true);
+        this.ajouteNavBar(root);
+        stage.setFullScreenExitHint("");
+        
+        scene.getStylesheets().add("styleAcceuil.css");
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+public void ajouteNavBar(BorderPane root){
+    // Barre de recherche
+    HBox haut = new HBox();
+    HBox navBar = new HBox();
+    HBox navBar2 = new HBox();
+
+    haut.getStyleClass().add("bottom-border");
+
+    
+    ImageView imageView = new ImageView(new Image("logo.png"));
+    imageView.setFitHeight(70);
+    imageView.setFitWidth(70);
+    haut.getChildren().add(imageView);
+
+    // Rectangle Bleu
+    HBox rectangleB = new HBox(10);
+    rectangleB.setStyle("-fx-background-color: #B5D6FD; -fx-border-color:black; -fx-border-radius: 0.8em; -fx-background-radius: 0.8em;");
+    rectangleB.setPadding(new Insets(5,5,5,5));
+    rectangleB.setMaxHeight(45);
+    rectangleB.setPrefWidth(400);
+
+    ImageView imageRecherche = new ImageView("imageRecherche.png");
+    imageRecherche.setFitHeight(30);
+    imageRecherche.setFitWidth(30);
+
+    TextField textFieldRecherche = new TextField();
+    textFieldRecherche.setPromptText("Nom d'un produit");
+    textFieldRecherche.getStyleClass().add("text-field");
+    textFieldRecherche.setPrefWidth(400);
+
+
+    GridPane gridPane = new GridPane();
+    gridPane.add(textFieldRecherche, 1,0 );
+    gridPane.add(imageRecherche, 0, 0);
+    rectangleB.getChildren().addAll(imageRecherche, textFieldRecherche);
+    haut.getChildren().add(rectangleB);
+
+    //ComboBox Categories
+    ComboBox<String> comboBox = new ComboBox<>();
+    comboBox.setCursor(Cursor.HAND);
+    comboBox.getItems().addAll("Tout", "Vêtement", "Chaussure", "Accessoire", "Electromenager", "Informatique", "Jeux", "Livre", "Musique", "Sport", "Vehicule","Ustensile Cuisine","Meuble","Outil");
+    comboBox.setPromptText("Categories Populaires");
+    comboBox.setStyle("-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 15px; -fx-border-radius: 10; -fx-background-radius: 10; -fx-border-color: black; -fx-border-width: 1;");
+    comboBox.setPrefHeight(40);
+    navBar2.getChildren().addAll(navBar,comboBox);
+
+    // Bouton deconnexion
+    HBox bouton= new HBox();
+    Button boutonMessage = new Button();
+    boutonMessage.setGraphic(new ImageView(new Image("message.png")));
+    boutonMessage.setCursor(Cursor.HAND);
+
+
+
+    Button boutonCloche = new Button();
+    boutonCloche.setGraphic(new ImageView(new Image("cloche.png")));
+    boutonCloche.setCursor(Cursor.HAND);
+
+    Button boutonPanier = new Button();
+    boutonPanier.setGraphic(new ImageView(new Image("panier.png")));
+    boutonPanier.setCursor(Cursor.HAND);
+
+    boutonMessage.setStyle("-fx-background-color: transparent;");
+    boutonCloche.setStyle("-fx-background-color: transparent;");
+    boutonPanier.setStyle("-fx-background-color: transparent;");
+    bouton.getChildren().addAll(boutonMessage, boutonCloche, boutonPanier);
+    bouton.setSpacing(7);
+    haut.getChildren().addAll(navBar2, bouton);
+    haut.setPadding(new Insets(10,0,10,10));
+    HBox.setMargin(navBar2, new Insets(15, 770, 0, 15));
+    HBox.setMargin(rectangleB, new Insets(15, 0, 10, 200));
+    HBox.setMargin(bouton, new Insets(15,0,0,0));
+    
+    root.setTop(haut);
+
+}
+
+
+public static void main(String[] args) {
+    launch(args);
+}
+}
