637eb7849ecb5097470947ea4f18faf35d482773
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-21 11:50:38 +0200

    factorisation code + modification date

--- src/EncherirBD.java
+++ src/EncherirBD.java
@@ -42,7 +42,10 @@ public class EncherirBD {
 
     public Enchere meilleurEnchere(int idve) throws SQLException, ParseException {
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yy:HH/mm/ss");
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd:hh/mm/ss");
+        UtilisateurBD utilBD = new UtilisateurBD(laConnexionMySQL);
+        VenteBD venBD = new VenteBD(laConnexionMySQL);
+
         Statement s = this.laConnexionMySQL.createStatement();
         ResultSet rs = s.executeQuery(
                 "select idve, idut, dateheure, montant, prixbase, prixmin, debutve, finve, idob, idst  from ENCHERIR natural join VENTE where idve = "
@@ -53,60 +56,9 @@ public class EncherirBD {
         Timestamp dateHeure = new Timestamp(rs.getDate(3).getTime());
         LocalDateTime dateTimeDateHeure = LocalDateTime.parse(dateHeure.toString(), inputFormatter);
         String dateHeureString = dateTimeDateHeure.format(outputFormatter);
-        Double montant = rs.getDouble(4);
-        Double prixbase = rs.getDouble(5);
-        Double prixmin = rs.getDouble(6);
-        Timestamp debutVe = new Timestamp(rs.getDate(7).getTime());
-        LocalDateTime dateTimeDebut = LocalDateTime.parse(debutVe.toString(), inputFormatter);
-        String debutVeString = dateTimeDebut.format(outputFormatter);
-        Timestamp finVe = new Timestamp(rs.getDate(8).getTime());
-        LocalDateTime dateTimeFin = LocalDateTime.parse(finVe.toString(), inputFormatter);
-        String finVeString = dateTimeFin.format(outputFormatter);
-        int idob = rs.getInt(9);
-        int idst = rs.getInt(10);
-
-        Statement s2 = this.laConnexionMySQL.createStatement();
-        ResultSet rs2 = s2.executeQuery("select nomob, descriptionob, idcat, idut from OBJET where idob = " + idob);
-
-        String nomob = rs2.getString(1);
-        String descob = rs2.getString(2);
-        int idcat = rs2.getInt(3);
-        int idutV = rs2.getInt(4);
-
-        Statement s4 = this.laConnexionMySQL.createStatement();
-        ResultSet rs4 = s4
-                .executeQuery("SELECT pseudout,emailut,mdput,activeut,idrole FROM UTILISATEUR where idut =" + idutV);
-        rs4.next();
-        String pseudoV = rs4.getString(1);
-        String emailV = rs4.getString(2);
-        String mdpV = rs4.getString(3);
-        String activeV = rs4.getString(4);
-        boolean actifV = false;
-        if (activeV.equals("O")) {
-            actifV = true;
-        }
-        int idRoleV = rs4.getInt(5);
-
-
-        Statement s3 = this.laConnexionMySQL.createStatement();
-        ResultSet rs3 = s3
-                .executeQuery("SELECT pseudout,emailut,mdput,activeut,idrole FROM UTILISATEUR where idut =" + idutA);
-        rs3.next();
-        String pseudoA = rs3.getString(1);
-        String emailA = rs3.getString(2);
-        String mdpA = rs3.getString(3);
-        String activeA = rs3.getString(4);
-        boolean actifA = false;
-        if (activeA.equals("O")) {
-            actifA = true;
-        }
-        int idRoleA = rs3.getInt(5);
-
-        Utilisateur newUtiliAcheteur = new Utilisateur(idutA, pseudoA, emailA, mdpA, actifA, idRoleA);
-        Utilisateur newUtiliVendeur = new Utilisateur(idRoleV, pseudoV, emailV, mdpV, actifV, idRoleV);
-        Objet objet =  new Objet(idob, descob, nomob, null, newUtiliVendeur, idcat);
-        Vente newVente = new Vente(idve, prixbase, prixmin, debutVeString, finVeString, idst, objet);
-        objet.setVente(newVente);
-        return new Enchere(newUtiliAcheteur, newVente, montant, dateHeureString);
+        Double montant = rs.getDouble(4);        
+        Utilisateur newUtiliAcheteur = utilBD.utilisateurParId(idutA);
+        Vente ve = venBD.venteParId(idve);
+        return new Enchere(newUtiliAcheteur, ve, montant, dateHeureString);
     }
 }
