1b6e2fd3cbb3cb6dc9262548ca9c136e8feccfa9
amelie_brizard <amelie.brizard@etu.univ-orleans.fr>
2023-06-21 10:23:12 +0200

    Modification du temps de connexion et amélioration du menu de gauche et des espacements

--- src/VueAdminGestionUtilisateurs.java
+++ src/VueAdminGestionUtilisateurs.java
@@ -17,6 +17,7 @@ import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.Cursor;
 
 public class VueAdminGestionUtilisateurs extends BorderPane {
     private Button boutonGestionUtil;
@@ -26,6 +27,9 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
     private ScrollPane scrollPaneProfils;
     private List<BorderPane> listeDesProfils;
 
+    /**
+     * Construction permettant de créer une nouvelle vue de gestion des utilisateurs.
+     */
     public VueAdminGestionUtilisateurs() {
         super();
         this.listeDesProfils = new ArrayList<>();
@@ -36,32 +40,31 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
         this.setCenter(partieCentrale);
         this.setStyle("-fx-background-color: white;");
         BorderPane.setMargin(titre, new Insets(20));
-        BorderPane.setMargin(partieCentrale, new Insets(10));
+        BorderPane.setMargin(partieCentrale, new Insets(0, 18, 0, 18));
         BorderPane.setAlignment(partieCentrale, Pos.TOP_CENTER);
+        this.getStylesheets().add("stylePageUtilisateur.css");
 
-
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
-        // this.ajouterUnProfil("test", 5);
+        // this.ajouterUnProfil("test1", 0);
+        // this.ajouterUnProfil("test2", 2);
+        // this.ajouterUnProfil("test3", 4);
+        // this.ajouterUnProfil("test4", 6);
+        // this.ajouterUnProfil("test5", 8);
+        // this.ajouterUnProfil("test6", 10);
+        // this.ajouterUnProfil("test7", 12);
+        // this.ajouterUnProfil("test8", 14);
+        // this.ajouterUnProfil("test9", 16);
+        // this.ajouterUnProfil("test10", 18);
+        // this.ajouterUnProfil("test11", 20);
+        // this.ajouterUnProfil("test12", 60);
+        // this.ajouterUnProfil("test13", 1440);
+        // this.ajouterUnProfil("test14", 8500);
         this.majDesProfils();
     }
 
     /**
-     * Méthode permettant d'ajouter un nouveau profil. À modifier si besoin.
-     * @param nom
-     * @param derniereConnexion
+     * Méthode permettant d'ajouter un nouveau profil (à modifier si besoin).
+     * @param nom String : le nom de l'utilisateur à ajouter.
+     * @param derniereConnexion int : sa dernière connexion.
      */
     public void ajouterUnProfil(String nom, int derniereConnexion) {
         BorderPane tempProfil = new BorderPane();
@@ -95,19 +98,20 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
         if (this.listeDesProfils.isEmpty()) {
             Text texteListeVide = new Text("Il n'y a aucun utilisateur existant.");
             texteListeVide.setFont(Font.font("Arial", 18));
-            this.scrollPaneProfils.setContent(texteListeVide);
+            tousLesProfils.setPadding(new Insets(15, 0, 0, 0));
+            tousLesProfils.getChildren().add(texteListeVide);
         }
         else {
             for (BorderPane profil : this.listeDesProfils) {
                 tousLesProfils.getChildren().add(profil);
             }
-            this.scrollPaneProfils.setContent(tousLesProfils);
         }
+        this.scrollPaneProfils.setContent(tousLesProfils);
     }
 
     /**
      * Méthode permettant de créer les boutons "supprimer" et "désactiver" du profil.
-     * @return
+     * @return VBox : la boite contenant les boutons "supprimer" et "désactiver".
      */
     private VBox getLesBoutonsProfil() {
         Button supprimer = new Button("Supprimer");
@@ -122,8 +126,8 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant de créer le nom d'utilisateur.
-     * @param nom
-     * @return
+     * @param nom String : le nom de l'utilisateur à créer.
+     * @return Text : le nom de l'utilisateur sous forme de Text.
      */
     private Text getNomDuProfil(String nom) {
         Text nomDuProfil = new Text(nom);
@@ -133,7 +137,7 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant de créer la photo de profil.
-     * @return
+     * @return ImageView : la photo de profil de l'utilisateur.
      */
     private ImageView getImageProfil() {
         Circle cercleImage = new Circle(40, 40, 40);
@@ -147,12 +151,25 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant de créer une boite contentant le nom de l'utilisateur et la dernière connexion.
-     * @param nomDuProfil
-     * @param derniereConnexion
-     * @return
+     * @param nomDuProfil Text : le nom de l'utilisateur.
+     * @param derniereConnexion int : sa dernière connexion (en minutes).
+     * @return VBox : une boite contenant les informations de profil de l'utilisateur (son pseudo et sa dernière connexion).
      */
     private VBox getInfosProfil(Text nomDuProfil, int derniereConnexion) {
-        VBox boiteInfosProfil = new VBox(10, nomDuProfil, new Label("Dernière connexion : il y a " + derniereConnexion + " (min/h/j)"));
+        String connexion = "Dernière connexion :";
+        VBox boiteInfosProfil = new VBox(10, nomDuProfil);
+        if (derniereConnexion == 0) {
+            boiteInfosProfil.getChildren().add(new Label(connexion + " à l'instant."));
+        }
+        else if (derniereConnexion < 60) {
+            boiteInfosProfil.getChildren().add(new Label(connexion + " il y a " + derniereConnexion + " min."));
+        }
+        else if (derniereConnexion >= 60 && derniereConnexion < 1440) {
+            boiteInfosProfil.getChildren().add(new Label(connexion + " il y a " + derniereConnexion/60 + " h."));
+        }
+        else {
+            boiteInfosProfil.getChildren().add(new Label(connexion + " il y a " + derniereConnexion/1440 + " j."));
+        }
         boiteInfosProfil.setPadding(new Insets(10));
         boiteInfosProfil.setStyle("-fx-background-color: #f1f1f1;");
         return boiteInfosProfil;
@@ -160,7 +177,7 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant d'initialiser le titre principal de la page.
-     * @return
+     * @return Text : le titre de la page.
      */
     private Text initTitrePage() {
         Text titrePrincipalPage = new Text("Administration");
@@ -170,51 +187,29 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant d'initialiser le bouton de gestion des utilisateurs.
-     * @return
      */
-    private StackPane initBoutonGestionUtil() {
-        this.boutonGestionUtil = new Button("Gestion des utilisateurs");
-        this.boutonGestionUtil.setPadding(new Insets(5));
-        StackPane boiteBoutonGestionUtil = new StackPane(this.boutonGestionUtil);
-        boiteBoutonGestionUtil.setPrefWidth(190);
-        boiteBoutonGestionUtil.setPadding(new Insets(5));
-        this.boutonGestionUtil.setStyle("-fx-background-color: #b5d6fd; -fx-background-radius: 0.8em; -fx-border-radius: 0.8em;");
-        StackPane.setAlignment(this.boutonGestionUtil, Pos.CENTER_LEFT);
-        return boiteBoutonGestionUtil;
+    private void initBoutonGestionUtil() {
+        this.boutonGestionUtil = new Button("Gestion des utilisateurs   >");
+        this.boutonGestionUtil.setPadding(new Insets(10));
+        this.boutonGestionUtil.getStyleClass().add("buttonBleu");
     }
 
     /**
      * Méthode permettant d'initialiser le bouton de gestion des ventes.
-     * @return
      */
-    private StackPane initBoutonGestionVentes() {
-        this.boutonGestionVentes = new Button("Gestion des ventes");
-        this.boutonGestionVentes.setPadding(new Insets(5));
-        Label fleche = new Label(">");
-        StackPane boiteBoutonGestionVentes = new StackPane(this.boutonGestionVentes, fleche);
-        boiteBoutonGestionVentes.setPadding(new Insets(5));
-        boiteBoutonGestionVentes.setStyle("-fx-background-color: white; -fx-background-radius: 0.8em; -fx-border-radius: 0.8em;");
-        this.boutonGestionVentes.setStyle("-fx-background-color: transparent;");
-        StackPane.setAlignment(fleche, Pos.CENTER_RIGHT);
-        StackPane.setAlignment(this.boutonGestionVentes, Pos.CENTER_LEFT);
-        return boiteBoutonGestionVentes;
+    private void initBoutonGestionVentes() {
+        this.boutonGestionVentes = new Button("Gestion des ventes          >");
+        this.boutonGestionVentes.setPadding(new Insets(10));
+        this.boutonGestionVentes.getStyleClass().add("buttonBlanc");
     }
 
     /**
      * Méthode permettant d'initialiser le bouton des récapitulatifs.
-     * @return
      */
-    private StackPane initBoutonRecapitulatifs() {
-        this.boutonRecapitulatifs = new Button("Récapitulatifs");
-        this.boutonRecapitulatifs.setPadding(new Insets(5));
-        Label fleche = new Label(">");
-        StackPane boiteBoutonRecap = new StackPane(this.boutonRecapitulatifs, fleche);
-        boiteBoutonRecap.setPadding(new Insets(5));
-        boiteBoutonRecap.setStyle("-fx-background-color: white; -fx-background-radius: 0.8em; -fx-border-radius: 0.8em;");
-        this.boutonRecapitulatifs.setStyle("-fx-background-color: transparent;");
-        StackPane.setAlignment(fleche, Pos.CENTER_RIGHT);
-        StackPane.setAlignment(this.boutonRecapitulatifs, Pos.CENTER_LEFT);
-        return boiteBoutonRecap;
+    private void initBoutonRecapitulatifs() {
+        this.boutonRecapitulatifs = new Button("Récapitulatifs                   >");
+        this.boutonRecapitulatifs.setPadding(new Insets(10));
+        this.boutonRecapitulatifs.getStyleClass().add("buttonBlanc");
     }
 
     /**
@@ -223,7 +218,8 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
     private void initComboBox() {
         this.comboBoxDerniereConnexion = new ComboBox<>();
         this.comboBoxDerniereConnexion.setStyle("-fx-background-color: white; -fx-background-radius : 0.8em; -fx-border-radius: 0.8em; -fx-border-color : lightgrey;");
-        this.comboBoxDerniereConnexion.getItems().addAll("< 5 mins", "> 10 mins");
+        this.comboBoxDerniereConnexion.getItems().addAll("Dernière connexion", "Connectés", "> 5 min", "> 10 min", "> 30 min", "> 1h", "> 1j");
+        this.comboBoxDerniereConnexion.getSelectionModel().selectFirst();
         this.comboBoxDerniereConnexion.setMaxHeight(40);
     }
 
@@ -234,42 +230,46 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
         this.scrollPaneProfils = new ScrollPane();
         this.scrollPaneProfils.setStyle("-fx-background: #fdfdfd; -fx-border-color: #dddddd; -fx-border-radius : 0.8em; -fx-background-radius: 0.8em;");
         this.scrollPaneProfils.setFitToWidth(true);
-        this.scrollPaneProfils.setPadding(new Insets(25));
+        this.scrollPaneProfils.setPadding(new Insets(0, 20, 0, 20));
     }
 
     /**
      * Méthode permettant de constituer le menu de gauche.
-     * @return
+     * @return VBox : le menu de gauche.
      */
     private VBox getMenu() {
-        VBox leMenu = new VBox(10);
+        VBox leMenu = new VBox(5);
+        this.initBoutonGestionUtil();
+        this.initBoutonGestionVentes();
+        this.initBoutonRecapitulatifs();
+        leMenu.setPrefWidth(268);
         leMenu.setPadding(new Insets(20));
-        leMenu.getChildren().addAll(this.initBoutonGestionUtil(), this.initBoutonGestionVentes(), this.initBoutonRecapitulatifs());
+        leMenu.getChildren().addAll(this.boutonGestionUtil, this.boutonGestionVentes, this.boutonRecapitulatifs);
         return leMenu;
     }
 
     /**
      * Méthode permettant de constituer la partie centrale de la page.
-     * @return
+     * @return BorderPane : la partie centrale de la page.
      */
     private BorderPane getPartieCentrale() {
         BorderPane leCentre = new BorderPane();
         leCentre.setPadding(new Insets(10));
-        leCentre.setMaxHeight(800);
+        leCentre.setMaxHeight(900);
         this.initScrollPaneProfils();
         HBox lesElemsDeRecherche = this.getElemsRecherche();
         BorderPane.setAlignment(lesElemsDeRecherche, Pos.CENTER);
         BorderPane.setAlignment(this.scrollPaneProfils, Pos.TOP_CENTER);
         leCentre.setTop(lesElemsDeRecherche);
         leCentre.setCenter(this.scrollPaneProfils);
-        BorderPane.setMargin(lesElemsDeRecherche, new Insets(10));
+        BorderPane.setMargin(lesElemsDeRecherche, new Insets(0, 0, 10, 0));
         BorderPane.setMargin(this.scrollPaneProfils, new Insets(10));
         return leCentre;
     }
 
     /**
      * Méthode permettant de créer le bouton de recherche.
-     * @return
+     * @return Button : le bouton de recherche.
      */
     private Button getBoutonRecherche() {
         ImageView imageRecherche = new ImageView(new Image("file:./img/recherche.png"));
@@ -283,19 +283,19 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant de créer la barre de recherche en elle-même.
-     * @return
+     * @return TextField : le champ de recherche.
      */
     private TextField getBarreDeRecherche() {
         TextField barreDeRecherche = new TextField("Nom d'un utilisateur");
         barreDeRecherche.setAlignment(Pos.CENTER_LEFT);
-        barreDeRecherche.setStyle("-fx-effect: dropshadow(gaussian, grey, 8, 0, 1, 1); -fx-background-radius: 0.8em;");
+        barreDeRecherche.setStyle("-fx-effect: dropshadow(gaussian, grey, 8, 0, 1, 1); -fx-background-radius: 0.8em; -fx-background-color: white;");
         barreDeRecherche.setPrefWidth(320);
         return barreDeRecherche;
     }
 
     /**
      * Méthode permettant de créer la boite, qui contient la barre de recherche et son bouton.
-     * @return
+     * @return HBox : le boite contenant le bouton et la barre de recherche.
      */
     private HBox getBoiteBarreDeRecherche() {
         TextField barreDeRech = this.getBarreDeRecherche();
@@ -311,12 +311,12 @@ public class VueAdminGestionUtilisateurs extends BorderPane {
 
     /**
      * Méthode permettant de constituer une boite, contenant la boite de la barre de recherche, avec la ComboBox de la dernière connexion.
-     * @return
+     * @return HBox : une boite contenant la barre de recherche et la ComboBox de temps de connexion.
      */
     private HBox getElemsRecherche() {
         HBox boiteCatRecherche = new HBox(5);
         boiteCatRecherche.setPadding(new Insets(10));
-        boiteCatRecherche.setMaxWidth(520);
+        boiteCatRecherche.setMaxWidth(600);
         this.initComboBox();
         HBox laBoiteDeRecherche = this.getBoiteBarreDeRecherche();
         laBoiteDeRecherche.setAlignment(Pos.CENTER);
