a7952a78a0ad419c04dd7af591cf1df06b578bce
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-21 10:59:27 +0200

    Modification des imports

--- src/UtilisateurBD.java
+++ src/UtilisateurBD.java
@@ -1,7 +1,9 @@
-import java.sql.*;
-import java.util.ArrayList;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
