500ee3c57b8cf4a29fa7c960bf53f92e9fccce89
Yohann Loiseau <yohann.loiseau@etu.univ-orleans.fr>
2023-06-22 07:23:56 +0000

    Modif ControleurNavBar pour afficher FenetreMessage

--- src/NavBar.java
+++ src/NavBar.java
@@ -1,171 +1,48 @@
-import javafx.scene.Cursor;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.Circle;
-import javafx.geometry.Insets;
 
-public class NavBar extends HBox {
+public class ControleurNavBar implements EventHandler<ActionEvent>{
+    
+    /**
+     * La vue FenetreCoInsc
+     */ 
+    //private FenetreCreationCompte vue;
 
-    // private Image profileImage;
-
-    /*
-     * @Override
-     * public void init() {
-     * Image profileImage = new Image("pp.jpeg");
-     * ImageView imagePP = new ImageView(profileImage);
-     * }
-     * 
-     * public void start(Stage stage) throws Exception{
-     * // Construction du graphe de scène
-     * BorderPane root = new BorderPane();
-     * Scene scene = new Scene(root);
-     * stage.setTitle("Fenetre Acceuil");
-     * stage.setFullScreen(true);
-     * this.ajouteNavBar(root);
-     * stage.setFullScreenExitHint("");
-     * 
-     * scene.getStylesheets().add("styleAcceuil.css");
-     * 
-     * stage.setScene(scene);
-     * stage.show();
-     * }
+    /**
+     * La vue de l'application
      */
-
     private AppliVae appli;
     private ConnexionMySQL connexionMySQL;
-    private TextField textFieldRecherche;
 
-    public NavBar(AppliVae appli, ConnexionMySQL connexionMySQL) {
+    public ControleurNavBar(AppliVae appli, ConnexionMySQL connexionMySQL){
         this.appli = appli;
         this.connexionMySQL = connexionMySQL;
-        ajouteNavBar();
     }
 
-    public void ajouteNavBar() {
-        // Image profileImage = new Image("message.png");
-        // ImageView imagePP = new ImageView(profileImage);
-
-        // Barre de recherche
-        HBox navBar = new HBox();
-        HBox navBar2 = new HBox();
-
-        this.getStyleClass().add("bottom-border");
-
-        Button boutonLogo = new Button();
-        ImageView imageView = new ImageView(new Image("file:img/logo.png"));
-        imageView.setFitHeight(70);
-        imageView.setFitWidth(70);
-        boutonLogo.setGraphic(imageView);
-        boutonLogo.setCursor(Cursor.HAND);
-        boutonLogo.setStyle("-fx-background-color: transparent;");
-        boutonLogo.setAccessibleText("Logo");
-        boutonLogo.setOnAction(new ControleurNavBar(this.appli, this.connexionMySQL));
-
-        this.getChildren().add(boutonLogo);
-
-        // Rectangle Bleu
-        HBox rectangleB = new HBox(10);
-        rectangleB.setStyle(
-                "-fx-background-color: #B5D6FD; -fx-border-color:black; -fx-border-radius: 0.8em; -fx-background-radius: 0.8em;");
-        rectangleB.setPadding(new Insets(5, 5, 5, 5));
-        rectangleB.setMaxHeight(45);
-        rectangleB.setPrefWidth(400);
-
-        ImageView imageRecherche = new ImageView("file:img/imageRecherche.png");
-        imageRecherche.setFitHeight(30);
-        imageRecherche.setFitWidth(30);
-        imageRecherche.setOnMouseClicked(new ControleurRechercheClick(this.appli, this));
-
-        this.textFieldRecherche = new TextField();
-        textFieldRecherche.setPromptText("Nom d'un produit");
-        textFieldRecherche.getStyleClass().add("text-field");
-        textFieldRecherche.setPrefWidth(400);
-        textFieldRecherche.setOnKeyReleased(new ControleurRechercheEntrer(this.appli, this));
-
-        GridPane gridPane = new GridPane();
-        gridPane.add(textFieldRecherche, 1, 0);
-        gridPane.add(imageRecherche, 0, 0);
-        rectangleB.getChildren().addAll(imageRecherche, textFieldRecherche);
-        this.getChildren().add(rectangleB);
-
-        // ComboBox Categories
-        ComboBox<String> comboBox = new ComboBox<>();
-        comboBox.setCursor(Cursor.HAND);
-        comboBox.getItems().addAll("Tout", "Vêtement", "Chaussure", "Accessoire", "Electromenager", "Informatique",
-                "Jeux", "Livre", "Musique", "Sport", "Vehicule", "Ustensile Cuisine", "Meuble", "Outil");
-        comboBox.setPromptText("Tout");
-        comboBox.setStyle(
-                "-fx-background-color: white; -fx-text-fill: black; -fx-font-size: 15px; -fx-border-radius: 10; -fx-background-radius: 10; -fx-border-color: black; -fx-border-width: 1;");
-        comboBox.setPrefHeight(45);
-        comboBox.setOnAction(new ControleurCategorie(this.appli));
-        navBar2.getChildren().addAll(navBar, comboBox);
-
-        // Bouton deconnexion
-        HBox bouton = new HBox();
-        Button boutonMessage = new Button();
-        boutonMessage.setGraphic(new ImageView(new Image("file:img/message.png")));
-        boutonMessage.setCursor(Cursor.HAND);
-        boutonMessage.setAccessibleText("Messagerie");
-        boutonMessage.setOnAction(new ControleurNavBar(this.appli, this.connexionMySQL));
-
-        Button boutonCloche = new Button();
-        boutonCloche.setGraphic(new ImageView(new Image("file:img/cloche.png")));
-        boutonCloche.setCursor(Cursor.HAND);
-        boutonCloche.setAccessibleText("Notifications");
-        boutonCloche.setOnAction(new ControleurNavBar(this.appli, this.connexionMySQL));
-
-        Button boutonPanier = new Button();
-        boutonPanier.setGraphic(new ImageView(new Image("file:img/panier.png")));
-        boutonPanier.setCursor(Cursor.HAND);
-        boutonPanier.setAccessibleText("Panier");
-        boutonPanier.setOnAction(new ControleurNavBar(this.appli, this.connexionMySQL));
-
-        boutonMessage.setStyle("-fx-background-color: transparent;");
-        boutonCloche.setStyle("-fx-background-color: transparent;");
-        boutonPanier.setStyle("-fx-background-color: transparent;");
-        bouton.getChildren().addAll(boutonMessage, boutonCloche, boutonPanier);
-        bouton.setSpacing(7);
-        this.getChildren().addAll(navBar2, bouton);
-        this.setPadding(new Insets(10, 0, 10, 10));
-        HBox.setMargin(navBar2, new Insets(15, 770, 0, 15));
-        HBox.setMargin(rectangleB, new Insets(15, 0, 10, 200));
-        HBox.setMargin(bouton, new Insets(15, 0, 0, 0));
-
-        Button boutonPhotoProfil = new Button();
-
-        // Image de profile a modifier un fois les page relié entre elle
-        Image profileImage = new Image("file:img/pp.jpeg");
-        // Créer un ImageView pour afficher l'image
-        ImageView imagePP = new ImageView(profileImage);
-        imagePP.setFitWidth(70); // Largeur de l'image
-        imagePP.setFitHeight(70); // Hauteur de l'image
-
-        // mettre l'image de profile un cercle
-        Circle clip = new Circle(30, 30, 30);
-        imagePP.setClip(clip);
-        // ajouter l'image de profile dans un StackPane
-        StackPane stack = new StackPane();
-        stack.getChildren().addAll(imagePP);
-        stack.setPadding(new Insets(0, 0, 0, 0));
-        stack.setStyle("-fx-background-color: transparent;");
-        bouton.getChildren().add(stack);
-        boutonPhotoProfil.setGraphic(imagePP);
-        boutonPhotoProfil.setCursor(Cursor.HAND);
-        boutonPhotoProfil.setStyle("-fx-background-color: transparent;");
-        boutonPhotoProfil.setAccessibleText("Profil Utilisateur");
-        boutonPhotoProfil.setOnAction(new ControleurNavBar(this.appli, this.connexionMySQL));
-        this.getChildren().add(boutonPhotoProfil);
-
-    }
-
-    public TextField getTextFieldRecherche() {
-        return this.textFieldRecherche;
-    }
+    /**
+     * L'action consiste à changer de fenêtre pour aller à la page de connexion
+     * @param actionEvent l'événement action
+     */
+	@Override
+	public void handle(ActionEvent actionEvent) {
+        Button boutonSource = (Button) actionEvent.getSource();
+        if (boutonSource.getAccessibleText().equals("Messagerie")){
+            this.appli.modeMessage();
+        }
+        else if (boutonSource.getAccessibleText().equals("Notifications")){
+            //this.appli.popUpNotif();
+            System.out.println("pop up notif");
+        }
+        else if (boutonSource.getAccessibleText().equals("Panier")){
+            this.appli.modeMiseEnVente();
+        }
+        else if (boutonSource.getAccessibleText().equals("Logo")){
+            this.appli.modeAccueil();
+        }
+        else if (boutonSource.getAccessibleText().equals("Profil Utilisateur")){
+            this.appli.modeProfilUtilisateur();
+        }
+	}
 
 }
