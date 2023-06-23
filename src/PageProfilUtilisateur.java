af6e241512841622ef259c338fb0f44ee7aaf9d9
daniel_moreira <daniel.moreira.41160@laposte.net>
2023-06-23 08:45:19 +0200

    Compression de code

--- src/PageProfilUtilisateur.java
+++ src/PageProfilUtilisateur.java
@@ -83,8 +83,7 @@ public class PageProfilUtilisateur extends BorderPane {
     }
 
     public void afficherPaiements() {
-        GridPane fenetrePaiements = new FenetrePagePaiements(this.appli, this.connexionMySQL);
-        this.setCenter(fenetrePaiements);
+        this.ajoutePagePayement();
         this.textInfos.getStyleClass().remove("buttonBleu");
         this.textMDP.getStyleClass().remove("buttonBleu");
         this.textPaiement.getStyleClass().add("buttonBleu");
@@ -182,6 +181,47 @@ public class PageProfilUtilisateur extends BorderPane {
         boutonValider.getStyleClass().add("buttonBleu");
         boutonValider.setOnAction(new ControleurProfilUtilisateurChangementMDP(this, this.connexionMySQL, utilisateur));
         gridPane.add(boutonValider, 1, 4);
+
+        this.setCenter(this.gridPane);
+    }
+
+    public void ajoutePagePayement() {
+        this.gridPane = new GridPane();
+        gridPane.setStyle("-fx-border-color: #D9D9D9; -fx-border-width: 3px; -fx-border-radius: 10; -fx-padding: 20px;");
+        BorderPane.setMargin(gridPane, new Insets(130, 100, 100, 30));
+        gridPane.setVgap(300);
+
+        gridPane.add(new Text("Moyens de paiement enregistrés :"), 0, 0);
+        VBox vboxBouton = new VBox();
+        Text text = new Text("Enregistrer un nouveau moyen de paiement :");
+
+        ImageView imageVisa = new ImageView("file:img/visa.png");
+        imageVisa.setPreserveRatio(true);
+        imageVisa.setFitWidth(150);
+        Button boutonVisa = new Button();
+        boutonVisa.setGraphic(imageVisa);
+        boutonVisa.setCursor(Cursor.HAND);
+        boutonVisa.setStyle("-fx-background-color: transparent;");
+
+        ImageView imagePaypal = new ImageView("file:img/paypal.png");
+        imagePaypal.setPreserveRatio(true);
+        imagePaypal.setFitWidth(160);
+        Button boutonPaypal = new Button();
+        boutonPaypal.setGraphic(imagePaypal);
+        boutonPaypal.setCursor(Cursor.HAND);
+        boutonPaypal.setStyle("-fx-background-color: transparent;");
+
+        ImageView imageMasterCard = new ImageView("file:img/masterCard.png");
+        imageMasterCard.setPreserveRatio(true);
+        imageMasterCard.setFitWidth(100);
+        Button boutonMasterCard = new Button();
+        boutonMasterCard.setGraphic(imageMasterCard);
+        boutonMasterCard.setCursor(Cursor.HAND);
+        boutonMasterCard.setStyle("-fx-background-color: transparent;");
+
+        vboxBouton.getChildren().addAll(text, boutonVisa, boutonPaypal, boutonMasterCard);
+        vboxBouton.setSpacing(30);
+        gridPane.add(vboxBouton, 0, 1);
 
         this.setCenter(this.gridPane);
     }
